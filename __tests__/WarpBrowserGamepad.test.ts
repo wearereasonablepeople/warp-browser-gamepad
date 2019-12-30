ebffc93d4088f4b5b6b02759f4b8c1a6dfe5a56c
Renan Borges <me@renan.ninja>
2019-12-30 16:23:50 +0100

    Add Tips section in the README

--- __tests__/WarpBrowserGamepad.test.ts
+++ __tests__/WarpBrowserGamepad.test.ts
@@ -1,10 +1,32 @@
 const Gamepads = require("../src/WarpBrowserGamepad").default;
 
 describe("WarpBrowserGamepad", () => {
-  test("should return the two available layouts in the LAYOUT static property", () => {
-    expect(Gamepads.LAYOUT).toEqual({
-      DEFAULT: Gamepads.LAYOUT.DEFAULT,
-      XBOX_ONE: Gamepads.LAYOUT.XBOX_ONE
+  describe("Static LAYOUT prop", () => {
+    test("should return the two available layouts", () => {
+      expect(Gamepads.LAYOUT).toEqual({
+        DEFAULT: Gamepads.LAYOUT.DEFAULT,
+        XBOX_ONE: Gamepads.LAYOUT.XBOX_ONE
+      });
+    });
+  });
+
+  describe("createButton", () => {
+    test("should return a new pressed button", () => {
+      const instance = new Gamepads();
+      expect(instance.createButton(true, "MyButton1")).toEqual({
+        pressed: true,
+        value: 1,
+        name: "MyButton1"
+      });
+    });
+
+    test("should return a new released button", () => {
+      const instance = new Gamepads();
+      expect(instance.createButton(false, "MyButton2")).toEqual({
+        pressed: false,
+        value: 0,
+        name: "MyButton2"
+      });
     });
   });
 });
