a64cef3db064b6b9842915d1efc63880e6866814
Renan Borges <me@renan.ninja>
2019-12-30 16:58:35 +0100

    Add tests for the methodss

--- __tests__/WarpBrowserGamepad.test.ts
+++ __tests__/WarpBrowserGamepad.test.ts
@@ -10,6 +10,20 @@ describe("WarpBrowserGamepad", () => {
     });
   });
 
+  describe("createState", () => {
+    test("should return an empty new gamepad state", () => {
+      const instance = new Gamepads();
+      expect(instance.createState()).toEqual(
+        Array(4).fill({
+          buttons: Array(Gamepads.LAYOUT.DEFAULT.length).fill(
+            instance.createButton(false)
+          ),
+          axes: null
+        })
+      );
+    });
+  });
+
   describe("createButton", () => {
     test("should return a new pressed button", () => {
       const instance = new Gamepads();
@@ -27,6 +41,76 @@ describe("WarpBrowserGamepad", () => {
         value: 0,
         name: "MyButton2"
       });
+    });
+  });
+
+  describe("getButtons", () => {
+    test("should NOT call the callback handler if the previous and current buttons are the same", () => {
+      const buttonHandler = jest.fn();
+      const instance = new Gamepads(buttonHandler);
+      const prevGamepadState = instance.createState()[0];
+      const nextGamepadState = instance.createState()[0];
+
+      const result = prevGamepadState.buttons.map(
+        instance.getButtons(nextGamepadState)
+      );
+
+      expect(result).toEqual(prevGamepadState.buttons);
+      expect(buttonHandler).not.toBeCalled();
+    });
+
+    test("should call the callback handler with the different button if the previous and current buttons are NOT the same", () => {
+      const buttonHandler = jest.fn();
+      const instance = new Gamepads(buttonHandler);
+      const prevGamepadState = instance.createState()[0];
+      const nextGamepadState = instance.createState()[0];
+      const newButton = instance.createButton(true, "BUTTON_1");
+
+      nextGamepadState.buttons[1] = newButton;
+
+      const result = prevGamepadState.buttons.map(
+        instance.getButtons(nextGamepadState)
+      );
+
+      expect(result).not.toEqual(prevGamepadState.buttons);
+      expect(buttonHandler).toBeCalledWith(newButton, nextGamepadState);
+    });
+  });
+
+  describe("getAxes", () => {
+    test("should NOT call the callback handler if the previous and current axes are the same", () => {
+      const axesHandler = jest.fn();
+      const instance = new Gamepads(undefined, axesHandler);
+
+      const prevGamepadState = instance.createState()[0];
+      prevGamepadState.axes = [1, 1, 0, 0];
+
+      const nextGamepadState = instance.createState()[0];
+      nextGamepadState.axes = [1, 1, 0, 0];
+
+      const result = instance.getAxes(nextGamepadState, prevGamepadState.axes);
+
+      expect(result).toEqual(prevGamepadState.axes);
+      expect(axesHandler).not.toBeCalled();
+    });
+
+    test("should call the callback handler with the different axes if the previous and current axes are NOT the same", () => {
+      const axesHandler = jest.fn();
+      const instance = new Gamepads(undefined, axesHandler);
+
+      const prevGamepadState = instance.createState()[0];
+      prevGamepadState.axes = [1, 1, 0, 0];
+
+      const nextGamepadState = instance.createState()[0];
+      nextGamepadState.axes = [0, 1, 0, 1];
+
+      const result = instance.getAxes(nextGamepadState, prevGamepadState.axes);
+
+      expect(result).not.toEqual(prevGamepadState.axes);
+      expect(axesHandler).toBeCalledWith(
+        nextGamepadState.axes,
+        nextGamepadState
+      );
     });
   });
 });
